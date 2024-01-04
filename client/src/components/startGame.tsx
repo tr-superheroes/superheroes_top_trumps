8cadaa8250d1c67301070fcf509341f192bd6587
Subha <subharamkie@gmail.com>
2024-01-04 11:28:53 +0000

    Merge: Merge to fetch data from api

--- client/src/components/startGame.tsx
+++ client/src/components/startGame.tsx
@@ -1,98 +1,18 @@
 import { useState,useEffect, createContext } from "react"
-import { FETCH_URL, FetchGameResponse,NO_OF_CARDS } from "../types/game.types";
+import { FETCH_URL, FetchGameResponse } from "../types/game.types";
 import { GameContainer } from "./gameContainer";
 
 export const GameContext = createContext<FetchGameResponse>([]);
 export const StartGame:React.FC =() =>{
     const [isNewGame,setIsNewGame] = useState(true);
     const [response,setResponse] = useState<FetchGameResponse>([]);
+    //const noOfCards = 7;
     const fetchCards = async() =>{
         try{
-            //const data = await fetch(FETCH_URL+noOfCards);
-            //const result = await data.json();
-            const result = {
-                "cards": [
-                  {
-                    "id": "429",
-                    "name": "Man-Wolf",
-                    "image": "https://www.superherodb.com/pictures2/portraits/10/100/91.jpg",
-                    "powerstats": {
-                      "intelligence": "63",
-                      "strength": "44",
-                      "speed": "35",
-                      "durability": "42",
-                      "power": "45",
-                      "combat": "70"
-                    }
-                  },
-                  {
-                    "id": "474",
-                    "name": "Moses Magnum",
-                    "image": "https://www.superherodb.com/pictures2/portraits/10/100/1020.jpg",
-                    "powerstats": {
-                      "intelligence": "75",
-                      "strength": "28",
-                      "speed": "12",
-                      "durability": "42",
-                      "power": "55",
-                      "combat": "56"
-                    }
-                  },
-                  {
-                    "id": "162",
-                    "name": "Carnage",
-                    "image": "https://www.superherodb.com/pictures2/portraits/10/100/187.jpg",
-                    "powerstats": {
-                      "intelligence": "63",
-                      "strength": "63",
-                      "speed": "70",
-                      "durability": "84",
-                      "power": "88",
-                      "combat": "90"
-                    }
-                  },
-                  {
-                    "id": "226",
-                    "name": "Doctor Strange",
-                    "image": "https://www.superherodb.com/pictures2/portraits/10/100/55.jpg",
-                    "powerstats": {
-                      "intelligence": "100",
-                      "strength": "10",
-                      "speed": "12",
-                      "durability": "84",
-                      "power": "100",
-                      "combat": "60"
-                    }
-                  },
-                  {
-                    "id": "584",
-                    "name": "Shadow King",
-                    "image": "https://www.superherodb.com/pictures2/portraits/10/100/121.jpg",
-                    "powerstats": {
-                      "intelligence": "75",
-                      "strength": "12",
-                      "speed": "27",
-                      "durability": "100",
-                      "power": "100",
-                      "combat": "75"
-                    }
-                  },
-                  {
-                    "id": "300",
-                    "name": "Green Goblin II",
-                    "image": "https://www.superherodb.com/pictures2/portraits/10/100/25.jpg",
-                    "powerstats": {
-                      "intelligence": "75",
-                      "strength": "55",
-                      "speed": "37",
-                      "durability": "50",
-                      "power": "44",
-                      "combat": "26"
-                    }
-                  }
-                ]
-              }
+            const data = await fetch(FETCH_URL);
+            const result = await data.json();
             setResponse(result.cards);
+            console.log(result.cards.length);
         }catch(error){
             console.log(error);
         }
