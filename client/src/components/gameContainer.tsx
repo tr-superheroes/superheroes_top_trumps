8339675887a4246200d5bc013fd3a38dc2ecc611
Subha <subharamkie@gmail.com>
2024-01-04 19:32:47 +0000

    Feat:Added state variable for 2 sets of cards, passed player top card and setting name from the data passed

--- client/src/components/gameContainer.tsx
+++ client/src/components/gameContainer.tsx
@@ -1,4 +1,4 @@
-import { useContext,createContext } from "react"
+import { useContext,createContext, useState } from "react"
 import { GameContext } from "./startGame"
 import { FetchGameResponse } from "../types/game.types";
 import { TopCardPlayer } from "./top-card-player";
@@ -6,15 +6,27 @@ import { TopCardPC } from "./top-card-pc";
 import { CardStackPC } from "./card-stack-pc";
 import { CardStackPlayer } from "./card-stack-player";
 
-export const PlayerCardArrayContext = createContext<FetchGameResponse>([]);
+//export const PlayerCardArrayContext = createContext<FetchGameResponse>([]);
 export const GameContainer:React.FC = () =>{
-    //load card containers for pc & player
     //load message container for showing scores/winner for each round
     //keep score in state
-    //show player name
     
     const allCardsArray = useContext(GameContext);
-/*
+    const playerCardsArray = allCardsArray.slice(0,allCardsArray.length/2);
+    const pcArray = allCardsArray.slice(allCardsArray.length/2+1,allCardsArray.length);
+
+    //use it like a stack and pop from top to 0, always starting at highest index
+    const [currentPlayerCardIndex,setCurrentPlayerCardIndex] = useState(playerCardsArray.length-1);
+    const [currentPCCardIndex,setCurrentPCCardIndex] = useState(pcArray.length-1);
+
+    const [scores,setScores] = useState({pc:0,player:0});
+    const [winner,SetWinner] = useState();
+
+    const handlePlay = () =>{
+        console.log('here');
+    }
+
+    /*
     const [turns, setTurns] = useState();
     scores: { pc: 0, player: 0};
     data: {pc: [{}, {}, ], player: [{}, {}, {}];
@@ -22,18 +34,18 @@ export const GameContainer:React.FC = () =>{
     next turn is winner
     */
     return (
+
+        <main className="main-layout">
+            <div className="card-container">
+                <TopCardPC/>
+                <CardStackPC/>
+            </div>
+            <div className="card-container">
+                <TopCardPlayer card={playerCardsArray[currentPlayerCardIndex]}/>
+                <CardStackPlayer/> {//pass the current player index+1 to be able to loop through
+                                    }
+            </div>
+        </main>  
         
-        <PlayerCardArrayContext.Provider value={allCardsArray.slice(0,allCardsArray.length/2)}>
-            <main className="main-layout">
-            <div className="card-container">
-            <TopCardPC/>
-            <CardStackPC/>
-            </div>
-            <div className="card-container">
-            <TopCardPlayer/>
-            <CardStackPlayer/>
-            </div>
-            </main>  
-        </PlayerCardArrayContext.Provider>
     )
 }
--- client/src/components/top-card-player.tsx
+++ client/src/components/top-card-player.tsx
@@ -1,12 +1,15 @@
+import { TrumpCard } from "../types/game.types"
 import { PowerstatsForm} from "./powerstats-form"
-
-export const TopCardPlayer:React.FC =() =>{
- 
+interface CardProps{
+  card:TrumpCard;
+}
+export const TopCardPlayer:React.FC<CardProps> =({card}) =>{
+  console.log('card here:'+card.name);
     return (
         <>
         <div className="hero hero__pc">
           <img className="hero__img" src="/../src/assets/images/hero2.png" />
-          <h2 className="hero__header">Abin Sur</h2>
+          <h2 className="hero__header">{card.name}</h2>
 
           <PowerstatsForm/>
           
