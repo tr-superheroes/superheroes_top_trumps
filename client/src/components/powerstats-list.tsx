98441864776133b7a873e74b575ff47d253b3b53
netcam22 <annettelesage@btinternet.com>
2024-01-04 17:52:55 +0000

    feat: separate out powerstats list from card

---
+++ client/src/components/powerstats-list.tsx
@@ -0,0 +1,17 @@
+export const PowerstatsList:React.FC =() =>{
+    return (
+        <>
+        <h3>Powerstats</h3>
+        <div className="hero__wrapper">
+            <ul className="hero__powerstats">
+            <li>Intelligence: 88</li>
+            <li>Strength: 28</li>
+            <li>Speed: 35</li>
+            <li>Durability: 65</li>
+            <li>Power: 100</li>
+            <li>Combat: 85</li>
+            </ul>
+        </div>
+        </>
+    )
+}
