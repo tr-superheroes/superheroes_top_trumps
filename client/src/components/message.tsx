a318f32bb62d6706e07dc21340003d2cb6042e0b
smithaagarwal <smitha.kotian@gmail.com>
2024-01-08 14:18:00 +0000

    feat:add logic to show a different colored bubble for winner message

--- client/src/components/message.tsx
+++ client/src/components/message.tsx
@@ -2,10 +2,11 @@ interface MessageProps{
     message:string;
     imgUrl:string;
     playerTurn: boolean;
+    showWinner:boolean;
 }
-export const MessageContainer:React.FC<MessageProps> =({message,imgUrl, playerTurn}) =>{
+export const MessageContainer:React.FC<MessageProps> =({message,imgUrl, playerTurn, showWinner}) =>{
     return (
-        <div className = {playerTurn?" splash splash--player":" splash splash--pc"}>
+        <div className = {showWinner?" splash splash--end":(playerTurn?" splash splash--player":" splash splash--pc")}>
             <div className = "splash__text">
             <p className = "splash__text--main">{message}</p>
             {
