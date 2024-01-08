ab53376e83bc29330943b5fe5e679fc538de052a
smithaagarwal <smitha.kotian@gmail.com>
2024-01-08 21:52:31 +0000

    test:add test for card component

---
+++ client/src/components/card.test.tsx
@@ -0,0 +1,16 @@
+import "@testing-library/jest-dom";
+import { Card, CardProps } from "./card";
+import { it, expect, describe } from "vitest";
+import { render, screen } from "@testing-library/react";
+
+describe("Test Card component", () => {
+  it("Test if image passed as props is present", () => {
+    const sampleCardProps: CardProps = {
+      cardImage: "/../src/assets/images/card3.png",
+      cssClass: "pc",
+    };
+    render(<Card {...sampleCardProps} />);
+    const img = screen.getByRole("img");
+    expect(img.className).toBe("pc");
+  });
+});
